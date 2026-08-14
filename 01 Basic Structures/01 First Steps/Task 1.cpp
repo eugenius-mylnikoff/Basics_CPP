1aaadca9450575dc1e22f501f38f082236f08003
Eugenius <108799072+eugenius-mylnikoff@users.noreply.github.com>
2026-08-15 01:26:48 +0200

    Update Task 1.cpp

--- 01 Basic Structures/01 First Steps/Task 1.cpp
+++ 01 Basic Structures/01 First Steps/Task 1.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main() {
-	std::cout << "C++ is a general-purpose programming language with a bias towards systems programming that\n";
-	std::cout << "  - is a better C\n";
-	std::cout << "  - supports data abstraction\n";
-	std::cout << "  - supports object-oriented programming\n";
-	std::cout << "  - supports generic programming.\n";
+	std::cout << "C++ is a general-purpose programming language with a bias towards systems programming that\n"
+		<< "  - is a better C\n"
+		<< "  - supports data abstraction\n"
+		<< "  - supports object-oriented programming\n"
+		<< "  - supports generic programming.\n";
 }
